b359602d7b6ab9e704812beb4a4b5e3f0f4b0d24
Aan Nur Wahidi <aannurwahidi117@gmail.com>
2022-12-10 14:36:43 +0700

    remove test from path

--- api/tests.py
+++ api/tests.py
@@ -1,3 +1,22 @@
 from django.test import TestCase
+from channels.generic.websocket import AsyncWebsocketConsumer
+import json
 
+from .models import *
 # Create your tests here.
+
+class FirebaseTest(AsyncWebsocketConsumer):
+    async def connect(self):
+        name = self.scope['url_route']['kwargs']['name']
+        
+        for i in range(1000):
+            lst = get_GMJI_data_firebase(name)
+            self.send(json.dumps({
+                'E_data':lst[0]['data'],
+                'N_data':lst[1]['data'],
+                'Z_data':lst[2]['data']
+                }))
+        await self.close()
+        
+    async def disconnect(self, code):
+        return await super().disconnect(code)
